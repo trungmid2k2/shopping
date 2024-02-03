e4961ed5ec0a4be40f98633afd8d9e025393e1fb
trungdt <trungdoan7373@gmail.com>
2024-02-03 17:37:12 +0700

    add number cart header

--- src/components/header/PagesHeader.js
+++ src/components/header/PagesHeader.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { BagFill, PersonFill } from "react-bootstrap-icons";
 import { NavLink } from "react-router-dom";
+import useCartStore from "../../store/cart";
 
 export default function PagesHeader() {
+  const items = useCartStore((state) => state.items);
   return (
     <>
       <ul className="flex text-[15px]">
@@ -16,10 +18,15 @@ export default function PagesHeader() {
             CONTACT US
           </a>
         </li>
-        <li className="px-[15px] pt-[3px]">
-          <NavLink className="hover:text-[#0084d6]" to="/cart">
-            <BagFill />
-          </NavLink>
+        <li className="px-[15px] pt-[3px] ">
+          <div className="relative w-[30px]">
+            <NavLink className="hover:text-[#0084d6]" to="/cart">
+              <BagFill />
+            </NavLink>
+            <div className="absolute top-[-13px] right-0 font-semibold text-black bg-white w-[20px] h-[20px] text-center rounded-[50%]">
+              {items.length}
+            </div>
+          </div>
         </li>
         <li className="px-[15px] pt-[3px]">
           <NavLink className="hover:text-[#0084d6]" to="profile">
